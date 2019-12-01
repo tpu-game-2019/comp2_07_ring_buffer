96123917188edc2f16097269e78d533d35406ea4
Kuryu86 <Kuryu86+github@gmail.com>
2019-12-01 20:38:09 +0900

    リングバッファ構造体と周辺関数の定義

--- main.c
+++ main.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
+#include <string.h>
+#define RING_SIZE 11
+#define ARRAY_LENGTH(x)(sizeof(x)/sizeof(x[0]))
+typedef struct 
+{
+	const char* Data[RING_SIZE];
+	int First;
+	int Last;
+}RING_BUFFER;
+void Init_RING_BUFFER(RING_BUFFER*RingBuffer)
+{
+	for (int i = 0; i < RING_SIZE; i++)
+		RingBuffer->Data[i] = NULL;
+	RingBuffer->First = 0;
+	RingBuffer->Last = 0;
+}
+void pushRingBuffer(RING_BUFFER*RingBuffer,const char* Data)
+{
+	if((RingBuffer->Last+1)%RING_SIZE==RingBuffer->First)
+		return ;
+	RingBuffer->Data[RingBuffer->Last] = Data;
+	RingBuffer->Last = (RingBuffer->Last + 1) % RING_SIZE;
+}
+const char* popRingBuffer(RING_BUFFER*RingBuffer)
+{
+	int f = RingBuffer->First;
+	RingBuffer->First = (RingBuffer->First + 1) % RING_SIZE;
+	return RingBuffer->Data[f];
+}
+void ShowRingBuffer(RING_BUFFER*RingBuffer)
+{
+	for (int i = 0; i <RING_SIZE-1; i++)
+	{
+		printf("%s",RingBuffer->Data[i]);
+		if (RING_SIZE - 1 == i + 1)
+			printf("\n");
+		else
+			printf(",");
+	}
 
+}
 int main(int argc, char *argv[]) 
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[7];
 
-    return 0;
+	while(fgets(str, sizeof(str), stdin)){
+        printf("%s", str);
+	}
+
+	return 0;
 }
